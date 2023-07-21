0eba895133cf155ec8e1fce37992d4598b7c4dce
Pranali <patilpranali0018@gmail.com>
2023-07-21 13:12:18 +0530

    Classes file addes

---
+++ Classes/OOP.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    //class Car
+    //{
+    //    string color = "red";
+    //    static void Main(string[] args)
+    //    {
+    //        //Class>>object>>attribute
+    //        Car MyObj1 = new Car();
+    //        Car MyObj2 = new Car();
+    //        Console.WriteLine(MyObj1.color);
+    //        Console.WriteLine(MyObj2.color);
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    //Class Members
+    //    class MyClass
+    //    {
+    //static void Main()
+    //{                     ------>For method
+    //    fullThrottle();
+    //}
+
+    // String color = "red";  //field
+    //int maxspeed = 200;    //field
+
+    //OR --->You can also leave the fields blank, and modify them when creating the object
+    //string color;
+    //int maxspeed;
+
+    //public static void fullThrottle()  //Method
+    //{
+    //    Console.WriteLine("The car is going as fast as it can");
+    //    Console.ReadLine();
+    //}
+
+    //for Fields
+    //    static void Main(string[] args)
+    //    {
+    //        MyClass myObj = new MyClass();
+    //        myObj.color = "Yellow";
+    //        myObj.maxspeed = 100;
+    //        Console.WriteLine(myObj.color);
+    //        Console.WriteLine(myObj.maxspeed);
+    //        Console.ReadLine();
+    //    }
+    //}
+
+
+    //Summarise Example
+
+    //class ProgLang
+    //{
+    //    string Developer;
+    //    int Year;
+    //    string Memory;
+    //    string Performance;
+
+    //    static void Main(string[] args)
+    //    {
+    //        ProgLang CSharp = new ProgLang();
+    //        CSharp.Developer = "Microsoft";
+    //        CSharp.Year = 2000;
+    //        CSharp.Memory = "Automatic";
+    //        CSharp.Performance = "Slower";
+
+    //        ProgLang CPlusPLus = new ProgLang();
+    //       CPlusPLus.Developer = "Bjarne Stroustrup";
+    //        CPlusPLus.Year = 1985;
+    //        CPlusPLus.Memory = "Manual";
+    //        CPlusPLus.Performance = "Faster";
+
+    //        Console.WriteLine("C# Developer: " + CSharp.Developer);
+    //        Console.WriteLine("C++ Developer: " + CPlusPLus.Developer);
+    //        Console.ReadLine();
+    //    }
+    //}
+
+
+}
